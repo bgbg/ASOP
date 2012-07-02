1a0e1e823e6f743af662d35a4bff5e345b052331
Boris Gorelik <boris@gorelik.net>
2012-07-02 09:17:03 +0300

    Added some example code

--- examples.py
+++ examples.py
@@ -1,9 +1,11 @@
-from matplotlib import pylab as plt
 import numpy as np
-
 import asop
 
+
+
+
 def rosenbrock((x, y)):
+
         '''Rosenbrock function
 
         Rosenbrock(x, y) = (1 - x)^2 + 100(y - x^2)^2
@@ -33,107 +35,140 @@ def humpFunction(v):
 
     '''
 
-def demoOptimizeFunction2params(TIMES, SIZE, func, strTitle=None):
+def demoOptimizeFunction2params(TIMES, SIZE,
+                                func,
+                                scalingFunc='auto',
+                                strTitle=None,
+                                bPlot=True):
+    ''' Demo minimization of a 2-D function
+
+    Minimize a 2D function and optionally plot the resulting landscape
+
+    Parameters
+    ------------------
+    TIMES: number
+        how many learning iterations should be performed
+    SIZE:    number
+        population size in each learning iteration
+    func:    callable
+        the function to be minimized
+    scalingFunc:     function or string
+        scaling function to be used during the learning,
+        None or "auto" (default)
+    strTitle:    string or None (default)
+        if `bPlot` is `True`, then this title will be used for the plot.
+        if `strTitle` is `None` (default), then an empty string will
+        be used as plot title
+
+    Returns
+    -----------------
+    The optimizer and a population of solutions (tuple of 2 elements)
+    '''
+
     if strTitle is None:
         strTitle = ''
+    from matplotlib import pylab as plt
 
     dimensions = [asop.variableTypes.ContinuousVariable(np.linspace(-2, 2, 1000),
-                                                   samplingStd=.05)
+                                                   samplingStd=.05,
+                                                   )
                   for i in range(2)] #@UnusedVariable
-    scalingFunc =  asop.scaling.TanhScaling(78, 0.015)
     optimizer = asop.ASOP(func, dimensions, scaling=scalingFunc)
     d0 = optimizer.dimensions[0]
     d1 = optimizer.dimensions[1]
 
     iteration = 0
-#    pop = optimizer.train(100, )
-#    pop0 = pop
     populations = []
-#    populations = [pop0]
-#    iteration += 1
-    fig  = plt.figure()
+    if bPlot:
+        fig  = plt.figure()
     R = 3
 
     for i in range(TIMES): #@UnusedVariable
         iteration += 1
         pop = optimizer.train(SIZE, 10)
         populations.append(pop)
-        ax = fig.add_subplot(R, 1, R)
-        ax.plot([iteration,]*2,
-            (np.percentile([p[1] for p in pop], 20),
-             np.percentile([p[1] for p in pop], 80)), '-k')
-        ax.plot(iteration, np.median([p[1] for p in pop]), 'ok')
+        if bPlot:
+            ax = fig.add_subplot(R, 1, R)
+            ax.plot([iteration,]*2,
+                (np.percentile([p[1] for p in pop], 20),
+                 np.percentile([p[1] for p in pop], 80)), '-k')
+            ax.plot(iteration, np.median([p[1] for p in pop]), 'ok')
 
-    ax = fig.add_subplot(R, 1, 1)
-    ax.plot(d0.x, d0.pdfValues, '-g')
-
-    ax = fig.add_subplot(R, 1, 2)
-    ax.plot(d1.x, d1.pdfValues, '-g')
-
-    for i in range(0): #@UnusedVariable
-        iteration += 1
-        pop = optimizer.train(100, 100)
-        populations.append(pop)
-        ax = fig.add_subplot(R, 1, R)
-        ax.plot([iteration,]*2,
-            (np.percentile([p[1] for p in pop], 20),
-             np.percentile([p[1] for p in pop], 80)), '-k')
-        ax.plot(iteration, np.median([p[1] for p in pop]), 'ok')
-
-
-    ax = fig.add_subplot(R, 1, 1)
-    ax.plot(d0.x, d0.pdfValues, '-b')
-
-    ax = fig.add_subplot(R, 1, 2)
-    ax.plot(d1.x, d1.pdfValues, '-b')
-
-    ax = fig.add_subplot(R, 1, R)
-    ax.set_xlim(0, iteration + 1)
-    ax.set_yscale('log')
-    plt.close(fig)
-
-
-
-    from mpl_toolkits.mplot3d import Axes3D
-    from matplotlib import cm
-    from matplotlib.colors import LogNorm
-
-    fig = plt.figure()
-    ax = Axes3D(fig, azim = -128, elev = 43)
-    s = .1
-    X = np.arange(-3, 3.+s, s)
-    Y = np.arange(-3, 3.+s, s)
-    X, Y = np.meshgrid(X, Y)
-    Z = map(func, zip(X, Y))
-    ax.plot_surface(X, Y, Z, rstride = 4, cstride = 4, norm = LogNorm(),
-                    cmap = cm.jet) #@UndefinedVariable
     pop = optimizer.sample(100)
     z = map(optimizer.func, pop)
     x = [p[0] for p in pop]
     y = [p[1] for p in pop]
-    ax.plot(x, y, z, '*k')
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.title('%s %d %d'%(strTitle, TIMES, SIZE))
-    fig.savefig('%s_%s_%s.png'%(asop.variableTypes.DEBUG_FLAG, TIMES, SIZE))
-#    plt.close(fig)
+    if bPlot:
+        ax = fig.add_subplot(R, 1, 1)
+        ax.plot(d0.x, d0.pdfValues, '-g')
+
+        ax = fig.add_subplot(R, 1, 2)
+        ax.plot(d1.x, d1.pdfValues, '-g')
+
+        for i in range(0): #@UnusedVariable
+            iteration += 1
+            pop = optimizer.train(100, 100)
+            populations.append(pop)
+            ax = fig.add_subplot(R, 1, R)
+            ax.plot([iteration,]*2,
+                (np.percentile([p[1] for p in pop], 20),
+                 np.percentile([p[1] for p in pop], 80)), '-k')
+            ax.plot(iteration, np.median([p[1] for p in pop]), 'ok')
+
+
+        ax = fig.add_subplot(R, 1, 1)
+        ax.plot(d0.x, d0.pdfValues, '-b')
+
+        ax = fig.add_subplot(R, 1, 2)
+        ax.plot(d1.x, d1.pdfValues, '-b')
+
+        ax = fig.add_subplot(R, 1, R)
+        ax.set_xlim(0, iteration + 1)
+        ax.set_yscale('log')
+        plt.close(fig)
+
+
+
+        from mpl_toolkits.mplot3d import Axes3D
+        from matplotlib import cm
+        from matplotlib.colors import LogNorm
+
+        fig = plt.figure()
+        ax = Axes3D(fig, azim = -128, elev = 43)
+        s = .1
+        X = np.arange(-3, 3.+s, s)
+        Y = np.arange(-3, 3.+s, s)
+        X, Y = np.meshgrid(X, Y)
+        Z = map(func, zip(X, Y))
+        ax.plot_surface(X, Y, Z, rstride = 4, cstride = 4, norm = LogNorm(),
+                        cmap = cm.jet) #@UndefinedVariable
+        ax.plot(x, y, z, '*k')
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.title('%s %d %d'%(strTitle, TIMES, SIZE))
+        fig.savefig('%s_%s.png'%(TIMES, SIZE))
     return (optimizer, z)
 
 
 if __name__ == '__main__':
-    for func in (rastrigin, rosenbrock, sines2Dfunc):
-        for i in (3,): # (1, 2, 3):
-            asop.variableTypes.DEBUG_FLAG = i
-            lValues = []
-            fig = plt.figure(100 + i)
-            ax = fig.add_subplot(111)
-            for (t, s) in [(1, 100)]: #[ (10, 100), (100, 10)]:
-                print (t, s)
-                optimizer, values = demoOptimizeFunction2params(t, s, func, func.__name__)
-                values.sort()
-                lValues.append(values)
-                ax.plot(values, '-', label='%s, %d - %d, %d'%(func.__name__,
-                                                              i, t, s))
+    from matplotlib import pylab as plt
+    for (ixF, func) in enumerate((rastrigin, rosenbrock, sines2Dfunc)):
+        fig = plt.figure(100 + ixF)
+        ax = fig.add_subplot(111)
+        lValues = []
+        for (t, s) in [(100, 10), (10, 100), (2, 500), (1, 1000)]: #[(1, 100), (10, 100), (100, 10)]:
+            print (t, s)
+            optimizer, values = demoOptimizeFunction2params(t, s,
+                func,
+                strTitle = '%s times=%d size=%d'%(
+                     func.__name__, t, s),
+                bPlot=False)
+            values.sort()
+            lValues.append(values)
+            ax.plot(values, '-',
+                    label='%s, - times %d, size %d'%(
+                                   func.__name__,
+                                          t, s))
             ax.legend(loc=0)
             ax.set_yscale('log')
 
--- test/testASOPClass.py
+++ test/testASOPClass.py
@@ -1,6 +1,7 @@
 
 import unittest
 import asop
+from asop import variableTypes
 ASOP = asop.ASOP
 import numpy as np
 
@@ -8,12 +9,25 @@ import numpy as np
 class TestInstatination(unittest.TestCase):
     '''Various functions that test object instatination'''
 
+    def setUp(self):
+
+        def funcOneParam(x):
+            return x * x
+
+        def funcTwoParams(x, y):
+            #Rosenbrock function
+            return(1 - x) ** 2 + 100 * (y - x**2)**2
+
+        def funcThreeParams(x, y, z):
+            return funcTwoParams(x, y) + z * z
+        self.lFunctions = [funcOneParam, funcTwoParams, funcThreeParams]
+
     def testDimensionsAsNone(self):
         '''Supply None as dimensions argument'''
         for (i, f) in enumerate(self.lFunctions):
             n = i + 1
             optimizer = ASOP(f)
-            self.assertTrue(optimizer.nDimensions == n)
+            self.assertTrue(len(optimizer.dimensions) == n)
 
 
     def testDimensionsAsNumber(self):
@@ -22,27 +36,53 @@ class TestInstatination(unittest.TestCase):
         for (i, f) in enumerate(self.lFunctions):
             n = i + 1
             optimizer = ASOP(f, n)
-            self.assertTrue(optimizer.nDimensions == n)
+            self.assertTrue(len(optimizer.dimensions) == n)
 
             optimizer = ASOP(f, str(n))
             self.assertTrue(optimizer.nDimensions == n)
 
     def testDimensionsAsVariables(self):
         '''Suppliy list of variables as dimensions argument'''
-        raise NotImplementedError()
+        for (i, f) in enumerate(self.lFunctions):
+            n = i + 1
+            variables = [variableTypes.ContinuousVariable()
+                         for j in range(n)] #@UnusedVariable
+            optimizer = ASOP(f, variables)
+            self.assertTrue(len(optimizer.dimensions) == n)
 
     def testDimensionsInvalidNumber(self):
         #supply negative int or non-int string
-        raise NotImplementedError()
+        for p in [-1, -2, 0, 1.2, '1.23']:
+            for f in self.lFunctions:
+                try:
+                    optimizer = ASOP(f, p) #@UnusedVariable
+                except ValueError:
+                    pass
+                else:
+                    self.fail()
+
 
     def testDimensionsInvalidObjectsInList(self):
         '''Supply invalid object in the dimenstions argument'''
-        raise NotImplementedError()
+        for (i, f) in enumerate(self.lFunctions):
+            n = i + 1
+            variables = [variableTypes.ContinuousVariable()
+                         for j in range(n)] #@UnusedVariable
+            variables[0] = None
+            try:
+                optimizer = ASOP(f, variables) #@UnusedVariable
+            except ValueError:
+                pass
+            else:
+                self.fail()
 
     def testDimensionsHaveNames(self):
         '''Dimensions created by default should have names'''
-        raise NotImplementedError()
-
+        for f in self.lFunctions:
+            optimizer = ASOP(f)
+            for dimension in optimizer.dimensions:
+                self.assertTrue(hasattr(dimension, 'name'))
+                self.assertTrue(bool(dimension.name))
 
 
 class TestFunctionality(unittest.TestCase):
@@ -51,13 +91,11 @@ class TestFunctionality(unittest.TestCase):
 
     def createDummyObject(self):
         '''Create a dummy ASOP object'''
-        raise NotImplementedError()
-
-
-    def testScalingWorks(self):
-        raise NotImplementedError()
-
-
+        def funcTwoParams(x, y):
+            #Rosenbrock function
+            return(1 - x) ** 2 + 100 * (y - x**2)**2
+        optimizer = ASOP(funcTwoParams)
+        return optimizer
 
     def testLearnArgumensDifferentLength(self):
         '''learn function accepts arguments with same length'''
@@ -73,8 +111,6 @@ class TestFunctionality(unittest.TestCase):
                 pass
             else:
                 self.fail()
-
-
 
     def testTrainFailsOnNegativeN(self):
         obj = self.createDummyObject()
@@ -99,29 +135,24 @@ class TestFunctionality(unittest.TestCase):
         #this is a stability test. sample many times
         #and make sure nothing crashes
         TIMES = 100
+        SIZE = 20
         obj = self.createDummyObject()
         for t in range(TIMES): #@UnusedVariable
-            n = np.logspace(0, 4).astype(int)
-            s = obj.sample(100)
-            self.assertTrue(len(s) == n)
+            for n in np.logspace(0, 4, SIZE).astype(int):
+                s = obj.sample(SIZE)
+                self.assertTrue(len(s) == n)
+
+
 
     def testTrainManyTimes(self):
         TIMES = 100
+        SIZE = 20
         obj = self.createDummyObject()
         for t in range(TIMES): #@UnusedVariable
-            n = np.logspace(0, 4).astype(int)
-            nToReturn = max(1, int(n / 2))
-            s = obj.train(n, nToReturn=nToReturn)
-            self.assertTrue(len(s) == nToReturn)
-
-            
-    def testAutoScalingIsSupported(self):
-        '''test scaling='auto' '''
-        raise NotImplementedError()
-    
-    
-
-
+            for n in np.logspace(0, 4, SIZE).astype(int):
+                nToReturn = max(1, int(n / 2))
+                s = obj.train(n, nToReturn=nToReturn)
+                self.assertTrue(len(s) == nToReturn)
 
 
 
